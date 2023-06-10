d339cb20d5812d923a7901ed3dd1e6b29189f67c
milanmajchrak <milan.majchrak@dataquest.sk>
2023-06-10 10:35:23 +0200

    Added a build scripts

---
+++ scripts/envs/__basic.bat
@@ -0,0 +1,5 @@
+rem Set those paths to relevant places in your computer, copy this file and rename the copy to "__basic.bat"
+set dspace_source=C:\mm\DSpace\
+set tomcat=C:\workspace\apps\apache-tomcat-9.0.53\
+set dspace_application=C:\dspace\
+set m2_source=%USERPROFILE%\.m2
